e35e95550d7728cf8ccd5fa6a6256be7149dbeb4
Alonso Urrea <45604888+AlonShaqed@users.noreply.github.com>
2019-03-21 15:31:58 -0600

    Update formulas.h

--- formulas.h
+++ formulas.h
@@ -1,6 +1,17 @@
 #include <math.h>
+#include <iostream>
+using namespace std;
 
 double Pi=3.14159265359;
+
+float mili(float n){return n/1000;}
+float centi(float n){return n/100;}
+float deci(float n){return n/10;}
+float giga(float n){return n*1000000000;}
+float mega(float n){return n*1000000;}
+float kilo(float n){return n*1000;}
+float hecto(float n){return n*100;}
+float deca(float n){return n*10;}
 
 float esfuerzo(float l_o, float l_f){
 	return (l_f-l_o)/l_o;
@@ -22,8 +33,9 @@ float area_pid(float d){
 	return (Pi*pow(d,2))/4;
 }
 
-float d_api(float a){
-	return sqrt(4*a/Pi);
+float d_api_meters(float a){
+	a=mili(a);
+	return sqrt((4*a)/Pi);
 }
 
 float f_factor_seg(float f, float fs){
@@ -31,7 +43,7 @@ float f_factor_seg(float f, float fs){
 }
 
 float l_f_se(float sigma, float e, float l_o){
-	return (sigma/e)*l_o+l_o;
+	return ((sigma/e)*l_o)+l_o;
 }
 
 float V(float e_x, float e_y){
@@ -40,4 +52,8 @@ float V(float e_x, float e_y){
 
 float ey_vex(float V, float e_x){
 	return V*e_x;
+}
+
+float phi_F(float phi, float e_y){
+	return phi-e_y;
 }
