4cd9a986876b328487a49f4c59e76572ec8aad9d
Alonso Urrea <45604888+AlonShaqed@users.noreply.github.com>
2019-03-21 15:32:36 -0600

    Update and rename menu.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,39 @@
 
 using namespace std;
 
-float func_b()
+float poisson()
 {
-	return 3.4;
+	float phi, l_o, area, f, sigma, l_f, e;
+	float V, e_y, e_x, phi_f;
+
+	cout<<"Longitud phi (cm) >"<<endl;
+	cin>>phi;
+	cout<<"Longitud l (cm) >"<<endl;
+	cin>>l_o;
+	cout<<"Fuerza (N) >"<<endl;
+	cin>>f;
+	cout<<"Coeficiente E (GPa) >"<<endl;
+	cin>>e;
+	cout<<"Coeficiete de Poisson [V] >"<<endl;
+	cin>>V;
+	cout<<"Coeficiente de Poisson [Ex] (e-05 m) >"<<endl;
+	cin>>e_x;
+
+	area=area_pid(centi(phi));
+	sigma=sigma_fa(f,area);
+	l_f=l_f_se(sigma, giga(e), centi(l_o));
+	cout<<"Longitud L final (m): "<<l_f<<endl;
+
+	e_y=ey_vex(V, mili(e_x)/100);
+	phi_f=phi_F(centi(phi),e_y);
+	cout<<"Longitud phi final (m): "<<phi_f<<endl;
+
 }
 
 float traccion()
 {
 	float e, sigma_o, sigma_ts, lo, df, lf, epsilon, s;
-	float f,area, diametro;
+	float f,area, diametro, fs, cfs;
 
 	cout<<"Modulo de Young (MPa)>"<<endl;
 	cin>>e;
@@ -25,6 +49,8 @@ float traccion()
 	cin>>df;
 	cout<<"==Diametro:\nCarga (kN)>"<<endl;
 	cin>>f;
+	cout<<"Factor de seguridad >"<<endl;
+	cin>>fs;
 
 	lf=lo+df;
 	epsilon=esfuerzo(lo,lf);
@@ -32,8 +58,11 @@ float traccion()
 	cout<<"Esfuerzo: "<<s<<endl;
 
 	area=area_fs(f, sigma_o);
-	diametro=d_api(area);
+	diametro=d_api_meters(area);
 	cout<<"Diametro (mm): "<<diametro<<endl;
+
+	cfs=f_factor_seg(f,fs);
+	cout<<"Factor de seguridad ("<<fs<<"):"<<cfs<<endl;
 }
 
 int select()
@@ -53,8 +82,8 @@ int menu()
 	switch(s)
 	{
 		case 1: ans=traccion(); break; //operacion uno, toma cuatro parametros numericos
-		case 2: ans=func_b(); break; //operacion dos igual a la primera
-		case 3: cout<<"Salir"<<endl;
+		case 2: ans=poisson(); break; //operacion dos igual a la primera
+		case 3: cout<<"Salir"<<endl; break;
 		default: cout<<"Opcion no elegible"<<endl;
 	}
 	
